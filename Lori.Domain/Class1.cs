f7b95784989f829e500bcb5dda3ddd694eb036ce
Exaurov Nikita <exaurov.nm@gmail.com>
2017-09-10 13:39:31 +0100

    Initial commit Base structure

---
+++ Lori.Domain/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lori.Domain
+{
+    public class Class1
+    {
+    }
+}
